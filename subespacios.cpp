cac62f672f65651903ee662b79a66e2b85af371d
JorgeRomojaro11 <jromodel@myuax.com>
2023-11-16 13:19:57 +0100

    .

--- subespacios.cpp
+++ subespacios.cpp
@@ -1,3 +1,34 @@
-//
-// Created by Jorge Romojaro on 16/11/2023.
-//
+#include <iostream>
+
+namespace Ciencia {
+    namespace Fisica {
+        const double velocidadLuz = 299792458;
+
+        double calcularEnergia(double masa) {
+            return masa * velocidadLuz * velocidadLuz;
+        }
+    }
+
+    namespace Quimica {
+        const double constanteAvogadro = 6.022e23;
+    }
+
+    namespace Matematicas {
+        const double constantePi = 3.14159265359;
+    }
+}
+
+int main() {
+    double masa;
+
+    std::cout << "Ingrese la masa en kg: ";
+    std::cin >> masa;
+
+    std::cout << "Energía correspondiente a una masa de " << masa << " kg: "
+              << Ciencia::Fisica::calcularEnergia(masa) << " Joules" << std::endl;
+
+    std::cout << "Constante de Avogadro: " << Ciencia::Quimica::constanteAvogadro << std::endl;
+    std::cout << "Valor de pi: " << Ciencia::Matematicas::constantePi << std::endl;
+
+    return 0;
+}
